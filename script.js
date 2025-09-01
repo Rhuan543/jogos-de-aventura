ee7cb6d802febbe470e3ed1858eec92dd8358be8
Rhuan543 <rhuan.freitas.lima@escola.pr.gov.br>
2025-09-01 14:14:58 -0300

    uma imagem

---
+++ script.js
@@ -0,0 +1,9 @@
+const avanca = document.querySelectorAll('btn.proximo');
+
+avanca.array.forEach(element => {
+    addButton.addeventlistener; click ,fuction()
+        const Atual = document.querySelector('ativo');
+        const proximoPasso = 'passo-'+ this.getattibute('data-proximo')
+         Atual.classList.remove('ativo');
+        document.get
+    })
